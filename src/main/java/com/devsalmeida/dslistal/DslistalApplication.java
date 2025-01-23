b10ad5c6f638b48d3ee1a45c9b2ce2521c815f7f
almdgustadev <gunna2513@gmail.com>
2025-01-23 18:59:09 -0300

    Projeto criado

---
+++ src/main/java/com/devsalmeida/dslistal/DslistalApplication.java
@@ -0,0 +1,13 @@
+package com.devsalmeida.dslistal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsalmeida/dslistal/DslistalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsalmeida.dslistal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
